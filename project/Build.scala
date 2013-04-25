831dfa2d7174095f4ad72eb10aebe34c9e8848bc
khalid <khalid.bouaabdalli@adisseo.com>
2013-04-25 17:09:28 +0200

    test

--- project/Build.scala
+++ project/Build.scala
@@ -16,10 +16,3 @@ object ApplicationBuild extends Build {
     )
 
 }
-
-object Plugins extends Build {
-  lazy val plugins = Project("plugins", file("."))
-    .dependsOn(
-      uri("git://github.com/bseibel/sbt-simple-junit-xml-reporter-plugin.git")
-    )
-}
