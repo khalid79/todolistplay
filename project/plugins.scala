27710e24fddd06646f8e5751f8e076b36af42f84
khalid <khalid.bouaabdalli@adisseo.com>
2013-05-02 17:39:14 +0200

    sbt-plugin 2.1.1

--- project/plugins.scala
+++ project/plugins.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Plugins extends Build {
-  lazy val plugins = Project("plugins", file("."))
+  /*lazy val plugins = Project("plugins", file("."))
     .dependsOn(
       uri("git://github.com/bseibel/sbt-simple-junit-xml-reporter-plugin.git")
-    )
+    )*/
 }
